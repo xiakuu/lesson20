c9af3a13793bcb08c22d6da951592f4deb0dce50
xiakuu <midnightcookie@mail.ru>
2023-08-28 20:49:56 +0300

    Initial commit

---
+++ second lesson/src/main/java/org/example/Block2Task1.java
@@ -0,0 +1,14 @@
+package org.example;
+
+import org.w3c.dom.ls.LSOutput;
+
+public class Block2 {
+    public static void main(String[] args) {
+        int b = 10;
+        int c = 15;
+
+        int a = 4*(b+c-1)/2;
+        System.out.println(a);
+    }
+
+}
---
+++ second lesson/src/main/java/org/example/NameAge.java
@@ -0,0 +1,8 @@
+package org.example;
+
+public class NameAge {
+    public static void main(String[] args) {
+        int age = 20;
+        System.out.println("Денис Савчук" + ", " + age);
+    }
+}
