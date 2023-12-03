08cb656d57b504611f24aea747552ad6fe62aece
xiakuu <100316445+xiakuu@users.noreply.github.com>
2023-12-04 00:47:48 +0300

    Delete second lesson directory

--- second lesson/src/main/java/org/example/NameAge.java
+++
@@ -1,8 +0,0 @@
-package org.example;
-
-public class NameAge {
-    public static void main(String[] args) {
-        int age = 20;
-        System.out.println("Денис Савчук" + ", " + age);
-    }
-}
